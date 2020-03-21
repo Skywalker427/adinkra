805346daffd2057c6868ac747fe397c70062042f
Hamzah Junaid Bawah <bhamza123@gmail.com>
2020-03-21 23:07:51 +0000

    Started work on symbol data

---
+++ symbols/symbols.js
@@ -0,0 +1,88 @@
+module.exports = [
+    {
+        name:"Adinkrahene", 
+        meaning:"Chief/King of the Adinkra Symbols", 
+        synopsis:`This symbol is said to have played an inspiring role in the designing of other symbols. it signifies the importance of playing a leadership role.` 
+        image:"adinkrahene.gif"
+    },
+    {
+        name:"Akoben", 
+        meaning:"War horn", 
+        synopsis:`Akoben is a horn used to sound a battle cry.`,
+        image:"akoben.gif"
+    },
+    {
+        name:"Akofena", 
+        meaning:"Sword of war", 
+        synopsis:`The crossed swords were a popular motif in the heraldic shields of many former Akan states. In addition to recognizing courage and valor, the swords can represent legitimate state authority.`,
+        image:"akofena.gif"
+    },
+    {
+        name:"Akoko Nan", 
+        meaning:"The leg of a hen", 
+        synopsis:`Symbol of nurturing and discipline
+        The full name of this symbol translates to "The hen treads on her chicks, but she does not kill them." This represents the ideal nature of parents, being both protective and corrective. An exhortation to nurture children, but a warning not to pamper them.`,
+        image:"akoko-nan.gif"
+    },
+    {
+        name:"Akoben", 
+        meaning:"War horn", 
+        synopsis:`Akoben is a horn used to sound a battle cry.`,
+        image:"akoben.gif"
+    },
+    {
+        name:"Akoma Ntoso", 
+        meaning:"Linked Hearts", 
+        synopsis:`Symbol of understanding and agreement.`,
+        image:"akoma-ntoso.gif"
+    },
+    {
+        name:"Akoma", 
+        meaning:"The Heart", 
+        synopsis:`Symbol of patience and tolerance
+        According to Agbo, when a person is said to "have a heart in his stomach," that person is very tolerant.`,
+        image:"akoma.gif"
+    },
+    {
+        name:"Ananse Ntontan", 
+        meaning:"Spider's Web", 
+        synopsis:`symbol of wisdom, creativity and the complexities of life
+        Ananse, the spider, is a well-known character in African folktales.`,
+        image:"ananse-ntontan.gif"
+    },
+    {
+        name:"Asase Ye Duru", 
+        meaning:"The Earth has weight", 
+        synopsis:`symbol of providence and the divinity of Mother Earth
+        This symbol represents the importance of the Earth in sustaining life.`,
+        image:"asase-ye-duru.gif"
+    },
+    {
+        name:"Aya", 
+        meaning:"Fern", 
+        synopsis:`symbol of endurance and resourcefulness
+        The fern is a hardy plant that can grow in difficult places. "An individual who wears this symbol suggests that he has endured many adversities and outlasted much difficulty." (Willis, The Adinkra Dictionary)`,
+        image:"aya.gif"
+    },
+    {
+        name:"Bese Saka", 
+        meaning:"sack of cola nuts", 
+        synopsis:`Symbol of affluence, power, abundance, plenty, togetherness and unity
+        The cola nut played an important role in the economic life of Ghana. A widely-used cash crop, it is closely associated with affluence and abundance. This symbol also represents the role of agriculture and trade in bringing peoples together.`,
+        image:"bese-saka.gif"
+    },
+    {
+        name:"Bi Nka Bi", 
+        meaning:"Let no one bit the other", 
+        synopsis:`symbol of peace and harmony
+        This symbol cautions against provocation and strife. The image is based on two fish biting each other tails.`,
+        image:"bi-nka-bi.gif"
+    },
+    {
+        name:"Boa Me Na Me Mmoa Wo", 
+        meaning:"Help me and let me help you", 
+        synopsis:`symbol of cooperation and interdependence`,
+        image:"akoben.gif"
+    },
+    
+]
