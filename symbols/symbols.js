3744d628994a90191c64f07fa53f18c858390ac0
Hamzah Junaid Bawah <bhamza123@gmail.com>
2020-03-22 20:19:46 +0000

    Updated symbols data

--- symbols/symbols.js
+++ symbols/symbols.js
@@ -2,7 +2,7 @@ module.exports = [
     {
         name:"Adinkrahene", 
         meaning:"Chief/King of the Adinkra Symbols", 
-        synopsis:`This symbol is said to have played an inspiring role in the designing of other symbols. it signifies the importance of playing a leadership role.` 
+        synopsis:`This symbol is said to have played an inspiring role in the designing of other symbols. it signifies the importance of playing a leadership role.` ,
         image:"adinkrahene.gif"
     },
     {
@@ -82,7 +82,349 @@ module.exports = [
         name:"Boa Me Na Me Mmoa Wo", 
         meaning:"Help me and let me help you", 
         synopsis:`symbol of cooperation and interdependence`,
-        image:"akoben.gif"
+        image:"boa-me-na-me-mmoa-wo.gif"
     },
-    
+    {
+        name:"Dame Dame", 
+        meaning:"name of a board game", 
+        synopsis:`symbol of intelligence and ingenuity`,
+        image:"dame-dame.gif"
+    },
+    {
+        name:"Denkyem", 
+        meaning:"Crocodile", 
+        synopsis:`The crocodile lives in the water, yet breathes the air, demonstrating an ability to adapt to circumstances.`,
+        image:"denkyem.gif"
+    },
+    {
+        name:"Duafe", 
+        meaning:"symbol of beauty and cleanliness; symbols of desirable feminine qualities", 
+        synopsis:`The meaning of this symbol is characterized slightly differently in "The Adinkra Dictionary" and "The Values of Adinkra Symbols"; the former emphasizes more abstract qualities of feminine goodness, love and care, while the latter has a more literal interpretation, looking one's best and good hygiene. In any case, the duafe was a prized possession of the Akan woman, used to comb and plait her hair.`,
+        image:"duafe.gif"
+    },
+    {
+        name:"Dwennimmen", 
+        meaning:"Ram's Horns", 
+        synopsis:`Symbol of humility together with strength
+        The ram will fight fiercely against an adversary, but it also submits humbly to slaughter, emphasizing that even the strong need to be humble.`,
+        image:"dwennimmen.gif"
+    },
+    {
+        name:"Eban", 
+        meaning:"Fence", 
+        synopsis:`Symbol of love, safety and security
+        The home to the Akan is a special place. A home which has a fence around it is considered to be an ideal residence.
+        The fence symbolically separates and secures the family from the outside. Because of the security and the protection that a fence affords, the symbol is also associated with the security and safety one finds in love.`,
+        image:"eban.gif"
+    },
+    {
+        name:"Epa", 
+        meaning:"Handcuffs", 
+        synopsis:`Symbol of law and justice, slavery and captivity
+        Adolph Agbo, in "Values of Adinkra Symbols" notes that handcuffs were introduced in Africa as a result of the slave trade, and later became popular among chiefs in cuffing offenders of the law. "The symbol reminds offenders of the uncompromising nature of the law. It however discourages all forms of slavery."`,
+        image:"epa.gif"
+    },
+    {
+        name:"Ese Ne Tekrema", 
+        meaning:"The teeth and tongue", 
+        synopsis:`symbol of friendship and interdependence
+        The teeth and the tongue play interdependent roles in the mouth. They may come into conflict, but they need to work together.`,
+        image:"ese-ne-tekrema.gif"
+    },
+    {
+        name:"Fawohodie", 
+        meaning:"Independence", 
+        synopsis:`Symbol of independence, freedom, emancipation
+        From the expression: Fawodhodie ene obre na enam.
+        Literal translation: "Independence comes with its responsibilities."`,
+        image:"fawohodie.gif"
+    },
+    {
+        name:"Fihankra", 
+        meaning:"House/Compound", 
+        synopsis:`Symbol of security and safety
+        Typical of Akan (Asante) architecture, the communal housing compound has only one entrance and exit.`,
+        image:"fihankra.gif"
+    },
+    {
+        name:"Fofo", 
+        meaning:"Yellow Flowered Plant", 
+        synopsis:`Symbol of jealousy and envy
+        "When the fofo's petals drop, they turn into black spiky-like seeds. The Akan liken the nature of this plant to a jealous person."
+        - The Adinkra Dictionary by W. Bruce Willis
+        There is a Akan proverb associated with this symbol: "What the fofo plant wishes is that the gyinantwi seeds turn black."`,
+        image:"fofo.gif"
+    },
+    {
+        name:"Funtunfunefu-Denkyemfunefu", 
+        meaning:"Siamese Crocodiles", 
+        synopsis:`Symbol of democracy and unity
+        The Siamese crocodiles share one stomach, yet they fight over food. This popular symbol is a remind that infighting and tribalism is harmful to all who engage in it.`,
+        image:"funtunfunefu-denkyemfunefu.gif"
+    },
+    {
+        name:"Gye Nyame", 
+        meaning:"Except for God", 
+        synopsis:`Symbol of the supremacy of God
+        This unique and beautiful symbol is ubiquitous in Ghana. It is by far the most popular for use in decoration, a reflection on the deeply religious character of the Ghanaian people`,
+        image:"gye-nyame.gif"
+    },
+    {
+        name:"Hwe Mu Dua", 
+        meaning:"Measuring Stick", 
+        synopsis:`Symbol of examination and quality control
+        This symbol stresses the need to strive for the best quality, whether in production of goods or in human endeavors.`,
+        image:"hwe-mu-dua.gif"
+    },
+    {
+        name:"Hye Won Hye", 
+        meaning:"That which does not burn", 
+        synopsis:`Symbol of imperishability and endurance
+        This symbol gets its meaning from traditional priests that were able to walk on fire without burning their feet, an inspiration to others to endure and overcome difficulties.`,
+        image:"hye-won-hye.gif"
+    },
+    {
+        name:"Kete Pa", 
+        meaning:"Good Bed", 
+        synopsis:`Symbol of a good marriage.
+        From the expression that a woman who has a good marriage is said to sleep on a good bed.`,
+        image:"kete-pa.gif"
+    },
+    {
+        name:"Kintinkantan", 
+        meaning:"Puffed-up extravagance", 
+        synopsis:`Symbol of arrogance`,
+        image:"kintinkantan.gif"
+    },
+    {
+        name:"Kwatakye Atiko", 
+        meaning:"Hair style of an Asante war captain", 
+        synopsis:`This symbols is said to be a special hair style of Kwatakye, a war captain of old Asante.
+        The symbol has come to represent bravery and fearlessness. It is also given as an earned title to any brave son of an Akan community.`,
+        image:"kwatakye-atiko.gif"
+    },
+    {
+        name:"Mate Masie", 
+        meaning:"What I hear, I keep", 
+        synopsis:`Symbol of wisdom, knowledge and prudence
+        The implied meaning of the phrase "mate masie" is "I understand". Understanding means wisdom and knowledge, but it also represents the prudence of taking into consideration what another person has said.`,
+        image:"mate-masie.gif"
+    },
+    {
+        name:"Me Ware Wo", 
+        meaning:"I shall marry you", 
+        synopsis:`Symbol of commitment, perseverance
+        From the expression "No one rushes into the job of mixing the concrete for building the house of marriage."`,
+        image:"me-ware-wo.gif"
+    },
+    {
+        name:"Mframadan", 
+        meaning:"Wind-resistant house", 
+        synopsis:`Symbol of fortitude and readiness to face life's vicissitudes
+        "This symbol suggests a reinforced or well-built home -- one built to withstand windy and treacherous conditions. It reflects in Asante history a clause in the unwritten constitutution of the Golden Stool. Oral acocunts say that according to that clause, mud houses in Kumasi must be reinforced with turf. This reinforcing would cause the house to be sturdier and resistant to unfavorable weather conditions."`,
+        image:"mframadan.gif"
+    },
+    {
+        name:"Mmere Dane", 
+        meaning:"Time changes", 
+        synopsis:`Symbol of change; life's dynamics`,
+        image:"mmere-dane.gif"
+    },
+    {
+        name:"Mmusuyidee", 
+        meaning:"That which removes bad luck", 
+        synopsis:`Symbol of good fortune and sanctity`,
+        image:"mmusuyidee.gif"
+    },
+    {
+        name:"Mpatapo", 
+        meaning:"Knot of pacification/reconciliation", 
+        synopsis:`Symbol of reconciliation, peacemaking and pacification
+        Mpatapo represents the bond or knot that binds parties in a dispute to a peaceful, harmonious reconciliation. It is a symbol of peacemaking after strife.`,
+        image:"mpatapo.gif"
+    },
+    {
+        name:"Mpuannum", 
+        meaning:"Five tufts", 
+        synopsis:`Symbol of priestly office, loyalty and adroitness
+        "This symbol is said to be the hairstyle of joy. It is the traditional hairstyle of the priestesses. ... The design of the adinkra symbol mpuannum resembles the way the priestesses' hair was tied. ...
+        It also represents the devotion and faithfulness one displays when doing a task required of one. In addition, mpuannum means loyalty or the embodiment of lofty duty to a desired goal."
+        - W. Bruce Willis, The Adinkra Dictionary`,
+        image:"mpuannum.gif"
+    },
+    {
+        name:"Nea Onnim So Sua A, Ohu", 
+        meaning:"He who does not know can know from learning", 
+        synopsis:`Symbol of knowledge, life-long education and continued quest for knowledge`,
+        image:"nea-onnim-so-sua-a-ohu.gif"
+    },
+    {
+        name:"Nea Ope Se Obedi Hene", 
+        meaning:"He who wants to be king", 
+        synopsis:`Symbol of service and leadership
+
+        From the expression "Nea ope se obedi hene daakye no, firi ase sue som ansa" meaning "He who wants to be king in the future must first learn to serve."`,
+        image:"nea-ope-se-obedi-hene.gif"
+    },
+    {
+        name:"Nkonsonkonson", 
+        meaning:"Chain link", 
+        synopsis:`Symbol of unity and human relations
+        A reminder to contribute to the community, that in unity lies strength`,
+        image:"nkonsonkonson.gif"
+    },
+    {
+        name:"Nkyimkyim", 
+        meaning:"Twisting", 
+        synopsis:`Symbol of initiative, dynamism and versatility`,
+        image:"nkyimkyim.gif"
+    },
+    {
+        name:"Nkyimu", 
+        meaning:"The crossed divisions made on adinkra cloth before stamping", 
+        synopsis:`Symbol of skillfulness and precision.
+        Before adinkra cloth is stamped with symbols, the artisan blocks off the cloth in a rectangular grid using a broad-tooth comb. This preparation is symbolic of the exacting technique which results in the highest quality product`,
+        image:"nkyimu.gif"
+    },
+    {
+        name:"Nsaa", 
+        meaning:"A type of hand-woven fabric", 
+        synopsis:`Symbol of excellence, genuineness, authenticity.
+        According to 'The Adinkra Dictionary' by W. Bruce Willis, Nsaa symbols reflects a saying: "nea onnim nsaa oton'ago", which translates as "He who does not know authentic Nsaa will buy the fakes". The quality of Nsaa has to represent quality of workmanship in general`,
+        image:"nsaa.gif"
+    },
+    {
+        name:"Nsoromma", 
+        meaning:"Child of the heavens/stars", 
+        synopsis:`Symbol of guardianship
+        A reminder that God is the father and watches over all people.`,
+        image:"nsoromma.gif"
+    },
+    {
+        name:"Nyame Biribi Wo Soro", 
+        meaning:"God is in the heavens", 
+        synopsis:`A symbol of hope. A reminder that God's dwelling place is in heaven, where he can listen to all prayers`,
+        image:"nyame-biribi-wo-soro.gif"
+    },
+    {
+        name:"Nyame Dua", 
+        meaning:"Tree of God/Altar", 
+        synopsis:`Symbol of God's presence and protection
+        The Nyame Dua is a sacred spot where rituals are performed. Erected in front of the house or compound, it is crafted from a tree that has been cut where three or more branches come together. This stake holds an earthenware vessell filled with water and herbs or other symbolic materials for purification and blessing rituals.`,
+        image:"nyame-dua.gif"
+    },
+    {
+        name:"Nyame Nnwu Na Mawu", 
+        meaning:"God never dies, therefore I cannot die", 
+        synopsis:`Symbol of God's omnipresence and the perpetual existence of man's spirit
+        This signifies the immortality of man's soul, believed to be a part of God. Because the soul rests with God after death, it cannot die.`,
+        image:"nyame-nnwu-na-mawu.gif"
+    },
+    {
+        name:"Nyame Nti", 
+        meaning:"By God's grace", 
+        synopsis:`Similar to Gye Nyame
+        According to The Adinkra Dictionary by W. Bruce Willis: "This stalk is depicted as the staff of life in many cultures. It symbolizes to the Akan that food is a basis of life and that they could not survive if not for the food that God has placed here on Earth for their nourishment."`,
+        image:"nyame-nti.gif"
+    },
+    {
+        name:"Nyame Ye Hene", 
+        meaning:"God is King", 
+        synopsis:`Symbol of the supremacy of God`,
+        image:"nyame-ye-hene.gif"
+    },
+    {
+        name:"Nyansapo", 
+        meaning:"Wisdom Knot", 
+        synopsis:`Symbol of wisdom, ingenuity, intelligence and patience.
+        An especially revered symbol of the Akan, this symbol conveys the idea that "a wise person has the capacity to choose the best means to attain a goal. Being wise implies broad knowledge, learning and experience, and the ability to apply such faculties to practical ends`,
+        image:"nyansapo.gif"
+    },
+    {
+        name:"Odo Nyera Fie Kwan", 
+        meaning:"Love never loses it's way home", 
+        synopsis:`Symbol of the power of love`,
+        image:"odo-nyera-fie-kwan.gif"
+    },
+    {
+        name:"Okodee Mmowere", 
+        meaning:"The talons of the eagle", 
+        synopsis:`Symbol of strength, bravery, power
+        The eagle is the mightiest bird in the sky, and its strength is concentrated in its talons. The Oyoko clan, one of the nine Akan clans, uses this symbol as their clan emblem.`,
+        image:"okodee-mmowere.gif"
+    },
+    {
+        name:"Onyankopon Adom Nti, Biribiara Beye Yie", 
+        meaning:"By God's grace, all will be well", 
+        synopsis:`Symbol of hope, providence, faith`,
+        image:"onyankopon-adom-nti-biribiara-beye-yie.gif"
+    },
+    {
+        name:"Osram Ne Nsoromma", 
+        meaning:"The moon and the star", 
+        synopsis:`Symbol of love, faithfulness, harmony
+        This symbol reflects the harmony that exists in the bonding between a man and a woman.
+        Proverb: "Kyekye pe aware." (The North Star has a deep love for marriage. She is always in the sky waiting for the return of the moon, her husband.)`,
+        image:"osram-ne-nsoromma.gif"
+    },
+    {
+        name:"Owo Foro Adobe", 
+        meaning:"Snake climbing raffia tree", 
+        synopsis:`Symbol of steadfastness, prudence and diligence
+        Because of its thorns, the raffia tree is a very dangerous challenge to the snake. His ability to climb it is a model of persistence and prudence.`,
+        image:"owo-foro-adobe.gif"
+    },
+    {
+        name:"Owuo Atwedee", 
+        meaning:"The ladder of death", 
+        synopsis:`Symbol of mortality
+        a reminder of the transitory nature of existence in this world and of the imperative to live a good life to be a worthy soul in the afterlife`,
+        image:"owuo-atwedee.gif"
+    },
+    {
+        name:"Pempamsie", 
+        meaning:"Sew in readiness", 
+        synopsis:`Symbol of readiness, steadfastness, hardiness
+        The design of this symbol resembles the links of a chain, and implies strength through unity as well as the importance of being prepared.`,
+        image:"pempamsie.gif"
+    },
+    {
+        name:"Sankofa", 
+        meaning:"Return and get it", 
+        synopsis:`Symbol of the importance of learning from the past`,
+        image:"sankofa.gif"
+    },
+    {
+        name:"Sesa Wo Suban", 
+        meaning:"Change or transform your character", 
+        synopsis:`Symbol of life transformation
+        This symbol combines two separate adinkra symbols, the "Morning Star" which can mean a new start to the day, placed inside the wheel, representing rotation or independent movement.`,
+        image:"sesa-wo-suban.gif"
+    },
+    {
+        name:"Tamfo Bebre", 
+        meaning:"The enemy will stew in his own juice", 
+        synopsis:`Symbol of jealousy and envy`,
+        image:"tamfo-bebre.gif"
+    },
+    {
+        name:"Wawa Aba", 
+        meaning:"Seed of the Wawa tree", 
+        synopsis:`Symbol of hardiness, toughness and perseverance
+        The seed of the wawa tree is extremely hard. In Akan culture, it is a symbol of someone who is strong and tough. It inspires the individual to persevere through hardship.`,
+        image:""
+    },
+    {
+        name:"Wo Nsa Da Mu A", 
+        meaning:"If your hands are in the dish", 
+        synopsis:`Symbol of participatory government, democracy and pluralism`,
+        image:"wo-nsa-da-mu-a.gif"
+    },
+    {
+        name:"Woforo Dua Pa A", 
+        meaning:"When you climb a good tree", 
+        synopsis:`Symbol of support, cooperation and encouragement
+        From the expression "Woforo dua pa a, na yepia wo" meaning "When you climb a good tree, you are given a push". More metaphorically, it means that when you work for a good cause, you will get support.`,
+        image:"woforo-dua-pa-a.gif"
+    }  
 ]
