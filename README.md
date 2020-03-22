9aca8dae0117ca5639897136acc93599efc6f82b
Hamzah Junaid Bawah <bhamza123@gmail.com>
2020-03-22 21:14:22 +0000

    Updated Readme

--- README.md
+++ README.md
@@ -4,3 +4,20 @@ Adinkra are symbols that represent concepts or aphorisms. Adinkra are used exten
 The symbols have a decorative function but also represent objects that encapsulate evocative messages that convey traditional wisdom, aspects of life or the environment.
 
 ## The API
+
+### Endpoints
+
+- GET /
+- GET /symbols
+
+#### Query Parameters
+
+The /symbols route accepts the query parameters name, meaning and synopsis for returning specific symbols that match those searches.
+
+To get all symbols with 'Nyame' in the name, you would send /symbols?name=Nyame
+
+To get all symbols with 'star' in the meaning you would send /symbols?meaning=star
+Same for star in the synopsis if you follow my drift.
+
+You can also combine query params. 
+/symbols?name=Nyame&meaning=heavens&synopsis=courage will return all symbols than contain 'Nyame' in the name or 'heaven' in the meaning or 'courage' in the synopsis.
--- routes/symbols.js
+++ routes/symbols.js
@@ -2,10 +2,40 @@ const express = require("express");
 const router = express.Router();
 let symbols = require("../symbols/symbols");
 
+
+
+
+
 router.get("/", (req,res,next)=>{
 
+    let symbolName = req.query.name;
+    let symbolMeaning = req.query.meaning;
+    let symbolSynopsis = req.query.synopsis;
+
+    let reponseSymbols = [];
+
+    if(symbolName){
+        symbols.forEach((symbol)=>{
+            if(symbol.name.toLowerCase().includes(symbolName.toLowerCase())){
+                reponseSymbols.push(symbol)
+            }
+        })
+    }
+    if(symbolMeaning){
+        symbols.forEach((symbol)=>{
+            if(symbol.meaning.toLowerCase().includes(symbolMeaning.toLowerCase())){
+                reponseSymbols.push(symbol)
+            }
+        })
+    }
+
+    if((symbolMeaning || symbolName) && reponseSymbols){
+        res.json(reponseSymbols)
+    }
+
+
     res.status(200)
-    .json(symbols);
+    .send(req.query.name);
 })
 
 module.exports = router;
